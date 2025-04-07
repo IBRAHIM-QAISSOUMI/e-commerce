e0ce81bed5cb6cfc09c7cf2013e69eaef379d3c7
Ibrahim <ibrahimqaissoumi@gmail.com>
2025-04-07 16:44:43 +0100

    some changes

---
+++ my-app/src/components/BestSellers.jsx
@@ -0,0 +1,28 @@
+import {useState, useEffect, useContext} from 'react'
+import Card from './Card'
+// import {products} from '../assets/frontend_assets/assets'
+import ProductsContext from '../Context/CreateContext'
+
+function BestSellers() {
+  const products = useContext(ProductsContext)
+
+  return (
+    <div className='Content'>
+        <h1>BEST SELLERS</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam velit architecto sunt </p>
+        <div className="product">
+            {products.filter(product => product.bestseller === true).map((product) => 
+            <Card 
+              key={product._id} 
+              image={product.image[0]}
+              price={product.price}
+              name={product.name}
+          /> )}
+        </div>
+    </div>
+  )
+}
+
+
+
+export default BestSellers
